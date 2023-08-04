415e4e1fd18c0d4140bbfc2ecb58ef7737508f28
May <maypham@utexas.edu>
2023-08-03 22:42:54 -0500

    fixed code, comments, and readme

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # Note-Taker
 UT's Fullstack Flex Web Development Bootcamp - Challenge 1`
+
+## Description
+This challenge was created to modify starter code to create an application where users can add notes and save them on the site. This app uses express.js, json file, and methods of routes to put, get, delete, and update files given specified links.
+
+## Usage
+![Screenshot](./assets/image.png)
+
+https://radiant-river-91151-6f38d8e44ed0.herokuapp.com/ 
+https://github.com/mayphamx/Note-Taker 
--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 // require express, api 
-// wildcard route defaults to page if user  not using link route
+// wildcard route -> page if user  not using link route
 // heroku
 const PORT = process.env.PORT || 3001;
 const path = require('path');
@@ -8,32 +8,29 @@ const express = require('express');
 const app = express();
 const Notes = require('./db/db.json');
 const { v4: uuidv4 } = require('uuid');
+
+// app.use statements
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use(express.static('public'));
-// this will put the data into the json
+// data for json notes
 app.get('/api/notes' , (req, res)=>{
     res.json(Notes);
 });
-// will send file into the html
+// read file from html
 app.get('/', (req, res) =>{
     res.sendFile(path.join(__dirname, './public/index.html'));
 });
 app.get('/notes', (req, res) =>{
   res.sendFile(path.join(__dirname, './public/notes.html'))
 });
-// wild card
+// wild card read index.html
 app.get('*', (req, res) =>{
     res.sendFile(path.join(__dirname, './public/index.html'));
 });
 // this function should create new tasks
 function createNewTasks(body, taskArray){
-    // const data = fs.readFileSync(path.join(__dirname, './db/db.json', 'utf8'))
     const newTask = body;
-    // if(!Array.isArray(taskArray));
-    // taskArray = [];
-    // if (taskArray.length === 0)
-    // taskArray.push(0);
     body.id = uuidv4();
     taskArray.push(body);
     console.log('data', body, taskArray)
@@ -44,6 +41,31 @@ app.post('/api/notes', (req, res) =>{
     const newTask = createNewTasks(req.body, Notes);
     res.json(newTask);
 });
+
+app.delete('/api/notes/:id', (req, res) => {
+    const noteIdToDelete = req.params.id;
+    const noteIndex = notes.findIndex((note) => note.id === noteIdToDelete);
+    if (noteIndex === -1) {
+      // erro return 404 Not Found
+      return res.status(404).json({ error: 'Note not found' });
+    }
+    // Remove the note from the notes array
+    notes.splice(noteIndex, 1);
+    // Update the db.json file with the updated notes array
+    fs.writeFile(
+      './db/db.json',
+      JSON.stringify(notes, null, 4),
+      (writeErr) => {
+        if (writeErr) {
+          console.error(writeErr);
+          return res.status(500).json({ error: 'Error when adding notes.' });
+        }
+        console.info('Note deleted.');
+        return res.json(notes);
+      }
+    );
+  });
+
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT}`)
 );
